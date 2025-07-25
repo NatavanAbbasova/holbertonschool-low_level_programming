59a65db842a46bd8a210deb95ce0e0426435d36e
root <11812@holbertonstudents.com>
2025-07-25 22:01:13 +0000

    I though that comments are just comments in the guidlines

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/**
+*main -Entry point of tje program
+*
+*Return: Always 0 (Succes)
+*/
 int main (void)
 {
-    printf("with proper grammar, but the outcome is a piece of art,\n");
-    return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
