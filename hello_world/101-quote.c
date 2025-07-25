bf52e10650e764af6329b04f181d9e13eb9478e9
root <11812@holbertonstudents.com>
2025-07-25 22:14:12 +0000

    still try to fix'em all

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,7 +1,13 @@
 #include <unistd.h>
+/**
+*main -Entry point of tje program
+*
+*Return: Always 0 (Succes)
+*/
 int main(void)
 {
-    const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(2, msg, sizeof(msg) - 1);
-    return (1);
+const char msg[] = "and that piece of art is useful\" - Dora Korpar, "
+"2015-10-19\n";
+write(2, msg, sizeof(msg) - 1);
+return (1);
 }
