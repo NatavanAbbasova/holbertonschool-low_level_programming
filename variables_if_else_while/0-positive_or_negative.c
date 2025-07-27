347f1d0347ebef38fa70d6f05cf332c9264e267c
root <11812@holbertonstudents.comgit push origin mainlsgit config --global user.email 11812@holbertonstudents.com>
2025-07-27 18:22:24 +0000

    positive negative numbers  5

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -2,9 +2,9 @@
 #include <time.h>
 #include <stdio.h>
 /**
-* more headers goes there
+* main - Entry point
 *
-*betty style doc for function main goes there
+*Return: Always 0 (Success)
 */
 int main(void)
 {
@@ -12,7 +12,7 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 /* your code goes there */
-if (n > 0) 
+if (n > 0)
 printf("%d is positive\n", n);
 else if (n == 0)
 printf("%d is zero\n", n);
