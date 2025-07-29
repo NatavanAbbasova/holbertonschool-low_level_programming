e75a03b16caa286ca3ac74d5bc7beba16decf55e
root <11812@holbertonstudents.com>
2025-07-29 21:00:04 +0000

    check for uppercase2

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
-* _ispper - checks for uppercase 
+*_ispper-checks for uppercase 
 *
-* Return 1 - uppercase, 0 otherwise 
+*Return(1)-uppercase, 0 otherwise 
 */
 int _isupper(int c)
 {
-if (c >= 'A' && c<='Z')
+if (c >= 'A' && c <= 'Z')
 return (1);
 else
 return (0);
