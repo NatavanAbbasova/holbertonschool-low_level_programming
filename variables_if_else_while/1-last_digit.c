e16d4b09316d23a7d5a80edee8bc88b200c3e18f
NatavanAbbasova <11812@holbertonstudents.com>
2025-07-30 09:57:54 +0000

    task

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -16,6 +16,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	last_digit = n % 10;
 	printf("Last digit of %d is %d ", n, last_digit);
+
 	if (last_digit > 5)
 	{
 		printf("and is greater than 5\n");
@@ -26,7 +27,7 @@ int main(void)
 	}
 	else
 	{
-		printf("and is less than 6 or not 0\n");
+		printf("and is less than 6 and not 0\n");
 	}
 	return (0);
 }
