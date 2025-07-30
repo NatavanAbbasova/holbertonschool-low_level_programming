fd4269e7fcb29aeff4ef5343af25b4f6b0d028f9
NatavanAbbasova <11812@holbertonstudents.com>
2025-07-30 10:24:57 +0000

    digit or letter

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isdigit imput for function
+ * @c variables
+ * Return 0 if c is a digit, else 1
+ */
+int _isdigit(int c)
+{
+    if (c >= 0 && c <= 9)
+        return (1);
+    else
+        return (0);
+}
