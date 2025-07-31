f8ec387ea238afb41ac047038aa6f95f216a93d9
NatavanAbbasova <11812@holbertonstudents.com>
2025-07-31 22:53:30 +0000

    staight line

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_line - draw a straight line
+ *@n lineeee integerrr
+ * Return: Always 0 (Succes)
+ */
+void print_line(int n)
+{
+	int x;
+
+	if (n <= 0)
+	{
+		_putchar('\n')
+		return;
+	}
+
+	for (x = 0; x < n; x++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
