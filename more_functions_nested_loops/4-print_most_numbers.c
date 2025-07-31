2472e864605e056eb24cb590368c3b2cbeaf2b79
NatavanAbbasova <11812@holbertonstudents.com>
2025-07-31 21:46:52 +0000

    except 2 and 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print_most_numbers - print numb but not 2-4
+*
+* Return: Always 0 (Succes)
+*/
+void print_most_numbers(void)
+{
+	int i:
+	for (i = 0; i <= 9; i++)
+	{
+		if (i != 2 && i != 4)
+			_putchar('0' + i);
+	}
+	_putchar('\n');
+}
