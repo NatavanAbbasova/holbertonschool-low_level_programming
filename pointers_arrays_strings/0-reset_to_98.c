f02d571cae060da84bbda6fdcc02dc3ccbdc00fa
NatavanAbbasova <11812@holbertonstudents.com>
2025-08-01 16:33:53 +0000

    98

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - take poi toint as 98
+ * @n: integer
+ * @p nedir bilmirem sualda var idi
+ * Return: nese
+ */
+void reset_to_98(int *n);
+int n = 98;
+int *p = &n;
+p = 402
