8d8f059e50138f150789c034e93cf529e92bf18b
NatavanAbbasova <11812@holbertonstudents.com>
2025-08-02 22:14:17 +0000

    uzunlugunu return ele

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - length of ne bilim e
+ * @s: input character ile
+ * Return: uzunluq olacaq
+ */
+int _strlen(char *s)
+
+{
+	int len = 0
+	while (*s)
+	{
+		s++;
+		len++;
+	}
+	
+	return (len);
+}
