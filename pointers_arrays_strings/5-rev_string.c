8c74b3ab8bf8b1d505e0a733ea6c279cf93beddf
NatavanAbbasova <11812@holbertonstudents.com>
2025-08-03 00:02:41 +0000

    reverse a string

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -2,11 +2,12 @@
 /**
  * rev_string - stringi cevirir
  * @s input olacey
+ * 
  * Description: niye yaziir tutmadim
  */
 void rev_string(char *s)
 {
-	int i = 0, int j = 0;
+	int i = 0, j = 0;
 	char hansisa;
 
 	while (s[j])
