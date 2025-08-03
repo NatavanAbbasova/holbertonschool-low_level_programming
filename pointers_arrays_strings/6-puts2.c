0948fcde4fdd5b2c7df6a5b465e7fccb245b3591
NatavanAbbasova <11812@holbertonstudents.com>
2025-08-03 00:40:13 +0000

    birini at birini put

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - bunlarikitabxanadan goturmek mumkun deyil?!
+ * @str: inputdudaaa 
+ *
+ * Description: yeni ki cut sirda duran herfleri
+ * put ele
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
