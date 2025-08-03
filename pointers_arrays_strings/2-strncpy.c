a79b3cae33d29d113969d9931bd2f12eec4cc7fa
NatavanAbbasova <11812@holbertonstudents.com>
2025-08-03 14:02:47 +0000

    copy edir

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strncpy - bayaqkinin eynisi amma bu cpoy edir
+ * @dest: getdiyi destination 
+ * @src: movcud oldugu source
+ * @n: kececek stringlerin sayi
+ *Return: return to desti
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+
+	return (dest);
+}
