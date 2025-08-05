1e25c799228e108782b0e38a297d35adad79d8bf
NatavanAbbasova <11812@holbertonstudents.com>
2025-08-05 23:55:21 +0000

    push eleyim bunu da

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - men de bunlari ciddi sanirdim
+ *
+ * Return: seni de :'(
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+		putchar(ch);
+	putchar('\n');
+
+	return (0);
+}
