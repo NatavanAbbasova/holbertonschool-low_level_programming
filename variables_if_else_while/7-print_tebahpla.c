070cc8f6b036dda8bab55ebd5bb892a088f9ae81
NatavanAbbasova <11812@holbertonstudents.com>
2025-08-05 23:53:26 +0000

    push eleyim bunu da

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - terse yaz
+ *
+ * Return: retuuuu
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+		putchar(ch);
+	putchar('\n');
+
+	return (0);
+}
