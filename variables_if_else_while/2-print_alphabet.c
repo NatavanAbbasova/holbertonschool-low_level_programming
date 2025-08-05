981e933094bbf3d6096f4884cf6f472e6321c735
NatavanAbbasova <11812@holbertonstudents.com>
2025-08-05 23:40:08 +0000

    axir ki basladim

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - main of function
+ *
+ * Return: return
+ */
+int main(void)
+{
+
+	char nese;
+
+	for (nese = 'a'; nese < z; nese++)
+		putchar(nese);
+	putchar('\n');
+	return (0);
+}
