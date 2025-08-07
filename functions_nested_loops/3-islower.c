7100729bb1dced9a30d37b1b0ca9dfd4420ab77e
NatavanAbbasova <11812@holbertonstudents.com>
2025-08-07 07:03:53 +0000

    neswe

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _islower - yoclayir ki if lowercase or not
+ */
+int _islower(int c)
+{
+	char c;
+	if ( i >= 'a'; i <= 'z')
+	{
+		return(1);
+	}
+	else
+		return(0);
+}
