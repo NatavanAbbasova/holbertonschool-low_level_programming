d73c9d8cd31bfbb61df11ccb06568ddc352a22c1
NatavanAbbasova <11812@holbertonstudents.com>
2025-08-07 06:11:51 +0000

    neswe

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - 10 dfe elifbani cap ele 
+ * @a: herfleri temsil edecek. yazmaq lazimdir goresen
+ * @i: integer sayi
+ */
+void print_alphabet_x10(void)
+{
+	char a = 'a';
+	for (int i = 0; int i <=10; i++)
+	{
+		while (a <= z)
+		{
+			putchar ('a');
+		{
+	putchar ('\n');
+	}
