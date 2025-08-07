e5922f36b72abf5ff1ebd44406dcd815862b79b2
NatavanAbbasova <11812@holbertonstudents.com>
2025-08-07 06:48:49 +0000

    neswe

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,22 +1,21 @@
 #include "main.h"
 /**
- * print_alphabet_x10 - 10 dfe elifbani cap ele 
- * @h: herfleri temsil edecek. yazmaq lazimdir goresen
- * @i: integer sayi
+ * print_alphabet_x10 - 10 defe cap edecek line
+ * @s: string olacaq
  */
 void print_alphabet_x10(void)
 {
-	char h = 'a';
-	int i;
+	char line[] = "abcdefghijklmnopqrstuvwxyz\n";
+	int i = 0;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 0; i < 10; i++)
 	{
-		while (h <= 'z')
+		int h = 0;
+		while (line[h] != '\0')
 		{
-			_putchar(h);
+			_putchar(line[h]);
 			h++;
 		}
-	_putchar ('\n');
-
+		_putchar('\n');
 	}
 }
