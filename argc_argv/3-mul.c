93b54a72a93259450eb5921ae8bef828630bf035
root <11812@holbertonstudents.com>
2025-08-10 18:09:01 +0000

    yeni bir raedme.md

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - men bu mul-u haradasa gormusem
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: return
+ */
+int main(int argc, char *argv[])
+{
+	int a, b, c;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	c = a * b;
+
+	printf("%d\n", c);
+
+	return (0);
+}
