92b4a3e1d766d314d4bbd11ff194c63ac12976fe
root <11812@holbertonstudents.com>
2025-08-15 22:36:13 +0000

    bfhdgkj

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include "main.h"
 /**
  * alloc_grid - allocates a 2D array of integers
  * @width: number of columns
@@ -19,18 +20,18 @@ int **alloc_grid(int width, int height)
 
 	for (i = 0; i < height; i++)
 	{
-	    grid[i] = malloc(sizeof(int) * width);
-	    if (grid[i] == NULL)
-	    {
-		    for (j = 0; j < i; j++)
-			    free(grid[j]);
-		    free(grid);
-		    return (NULL);
-	    }
-	    for (j = 0; j < width; j++)
-		    grid[i][j] = 0;
+		grid[i] = malloc(sizeof(int) * width);
+		if (grid[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+				free(grid[j]);
+			free(grid);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+			grid[i][j] = 0;
 	}
-
+	
 	return (grid);
 }
 
